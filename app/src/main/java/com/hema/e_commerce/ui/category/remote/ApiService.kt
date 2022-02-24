bcc3190ab8667066ea6306245839efc9d4723d7d
AyaAlaa010 <ayaalaa010920@gmail.com>
2022-02-24 23:33:52 +0200

    static categories 2

---
+++ app/src/main/java/com/hema/e_commerce/ui/category/remote/ApiService.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapplication.data.remote
+
+import android.telecom.Call
+import okhttp3.Response
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+//@GET("")
+//suspend   fun getAllCategories(
+//
+//    @Query("appid") key: String
+//
+//): Response<AllResponse>
+
+}
