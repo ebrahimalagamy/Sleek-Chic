cad22b4f66284b7d729caa52e4a04eb556cfd9e2
AyaAlaa010 <ayaalaa010920@gmail.com>
2022-02-25 17:59:58 +0200

    static categories 3

---
+++ app/src/main/java/com/hema/e_commerce/ui/category/categoryui/maincategoryui/CategoriesProductAdapter.kt
@@ -0,0 +1,98 @@
+package com.hema.e_commerce.ui.category.categoryui.maincategoryui
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.hema.e_commerce.R
+import com.hema.e_commerce.databinding.CategoriesProductsItemsBinding
+import com.hema.e_commerce.ui.category.categoryui.containerui.FragmentContainer
+import com.hema.e_commerce.ui.category.testmodels.model
+import androidx.fragment.app.FragmentManager as FragmentManager
+
+
+class CategoriesProductAdapter(
+    var  fragMan: FragmentManager?,
+    val productList: ArrayList<model>,
+    val context: Context
+) :
+        RecyclerView.Adapter<CategoriesProductAdapter.ViewHolder>() {
+    lateinit var navController:NavController
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            navController= Navigation.findNavController(parent)
+
+            return ViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.categories_products_items, parent, false))
+
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            Log.i("mmmmmmmmmmmmmmmmmmmmmm", "onBindViewHolder: "+productList.size)
+            val products = productList.get(position)
+            holder.itemBinding.productTypeName.text= products.name
+
+            holder.itemBinding.productTypeName.setOnClickListener{
+
+               // navController.navigate(R.id.action_category_to_fragmentContainer)
+
+
+
+                var fragContainer :FragmentContainer=FragmentContainer()
+//                var  bundle:Bundle= Bundle()
+//                bundle.putString("selected data",products.name)
+//
+//                fragContainer.arguments=bundle
+                //    FragmentManager().beginTransaction().replace(R.id.fram_cont, fragContainer)
+                val  manager: FragmentManager
+                fragMan!!.beginTransaction().replace(R.id.fram_cont, fragContainer).commit()
+
+
+            }
+
+
+        }
+
+        override fun getItemCount(): Int {
+            return productList.size
+        }
+
+
+        class ViewHolder(itemBinding: CategoriesProductsItemsBinding) : RecyclerView.ViewHolder(itemBinding.getRoot()) {
+            var itemBinding: CategoriesProductsItemsBinding
+
+            init {
+                this.itemBinding = itemBinding
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/hema/e_commerce/ui/category/categoryui/typelistofproduct/TypeListProductsFragment.kt
@@ -0,0 +1,49 @@
+package com.hema.e_commerce.ui.category.categoryui.typelistofproduct
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import com.hema.e_commerce.R
+import com.hema.e_commerce.databinding.FragmentContainerBinding
+import com.hema.e_commerce.databinding.FragmentTypeListProductBinding
+import com.hema.e_commerce.ui.category.categoryui.containerui.ContainerAdapter
+import com.hema.e_commerce.ui.category.repository.Repository
+import com.hema.e_commerce.ui.category.testmodels.ModelContainer
+import com.hema.e_commerce.ui.category.testmodels.TypeModelList
+
+
+class TypeListProductsFragment : Fragment() {
+  lateinit var binding:FragmentTypeListProductBinding
+    lateinit var adapter:TypeListAdapter
+    lateinit var arr:ArrayList<TypeModelList>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_type_list_product, container, false)
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arr= Repository().arrTypeList
+        adapter = TypeListAdapter(arr, requireContext())
+        val layoutManager = GridLayoutManager(requireContext(), 2 )
+        binding.recListProduct.adapter=adapter
+        binding.recListProduct.layoutManager=layoutManager
+
+
+
+
+    }
+}
