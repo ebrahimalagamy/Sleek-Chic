b0deff87aa35971a876caf06af79b0d4e41c6a7f
MohamedKamal16 <84516222+MohamedKamal16@users.noreply.github.com>
2022-03-10 22:52:31 +0200

    filterAgain

--- app/src/main/java/com/hema/e_commerce/ui/search/SearchFragment.kt
+++ app/src/main/java/com/hema/e_commerce/ui/search/SearchFragment.kt
@@ -2,7 +2,12 @@ package com.hema.e_commerce.ui.search
 
 import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import android.view.*
+import android.widget.AdapterView
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
@@ -10,6 +15,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.hema.e_commerce.R
+import com.hema.e_commerce.adapter.productList.TypeListAdapter
 import com.hema.e_commerce.adapter.search.SearchAdapter
 import com.hema.e_commerce.databinding.FragmentSearchBinding
 import com.hema.e_commerce.model.dataclass.allProducts.Product
@@ -24,8 +30,10 @@ class SearchFragment : Fragment() {
     lateinit var binding: FragmentSearchBinding
     lateinit var adapter: SearchAdapter
     lateinit var viewModel: ListOfProductsViewModel
-    lateinit var products:List<Product>
-     var searchProducts:ArrayList<Product> = arrayListOf()
+    lateinit var products: List<Product>
+    var searchProducts: ArrayList<Product> = arrayListOf()
+    private lateinit var seekBar:SeekBar
+    private lateinit var price:String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,70 +44,39 @@ class SearchFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
         binding.searchView.isIconified = false
 
-        val repository= Repository(RoomData(requireContext()))
+        val repository = Repository(RoomData(requireContext()))
         val listOfProductsViewModelProviderFactory =
-            ListOfProductViewModelFactory(requireActivity().application,repository)
-        viewModel = ViewModelProvider(this, listOfProductsViewModelProviderFactory)[ListOfProductsViewModel::class.java]
+            ListOfProductViewModelFactory(requireActivity().application, repository)
+        viewModel = ViewModelProvider(
+            this,
+            listOfProductsViewModelProviderFactory
+        )[ListOfProductsViewModel::class.java]
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         brandList()
-        binding.searchView.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                //win press on search not to crush so repeat the logic of text change
-                searchProducts.clear()
-                val searchText= query!!.lowercase(Locale.getDefault())
-                if (searchText.isNotEmpty()){
-                    products.forEach {
-                        if (it.title.lowercase(Locale.getDefault()).contains(searchText)
-                            || it.vendor.lowercase(Locale.getDefault()).contains(searchText)){
-                            searchProducts.add(it)
-                        }
-                    }
-                    brandList()
-                }else{
-                    searchProducts.clear()
-                }
+        search()
 
-                return false
-            }
+//        click()
+        filter()
 
-            override fun onQueryTextChange(newText: String?): Boolean {
-                //change when writing this the main logic
-                searchProducts.clear()
-                val searchText= newText!!.lowercase(Locale.getDefault())
-                if (searchText.isNotEmpty()){
-                    products.forEach {
-                        if (it.title.lowercase(Locale.getDefault()).contains(searchText)
-                            || it.vendor.lowercase(Locale.getDefault()).contains(searchText)){
-                            searchProducts.add(it)
-                        }
-                    }
-                    brandList()
-                }else{
-                    searchProducts.clear()
-                }
 
-            return false
-            }
-
-        })
 
     }
 
 
-
-//call all product
-    fun brandList(){
+    //call all product
+    fun brandList() {
         initViews()
         observeSearchProduct()
     }
 
     private fun observeSearchProduct() {
-        viewModel.allProduct .observe(viewLifecycleOwner, Observer {
-            products= it.products
+        viewModel.allProduct.observe(viewLifecycleOwner, Observer {
+            products = it.products
             adapter = SearchAdapter(searchProducts)
             val layoutManager = GridLayoutManager(requireContext(), 2)
             binding.recListProduct.adapter = adapter
@@ -111,5 +88,146 @@ class SearchFragment : Fragment() {
     private fun initViews() {
         viewModel.getProducts()
     }
-    
+
+    private fun search(){
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //win press on search not to crush so repeat the logic of text change
+                searchProducts.clear()
+                val searchText = query!!.lowercase(Locale.getDefault())
+                if (searchText.isNotEmpty()) {
+                    products.forEach {
+                        if (it.title.lowercase(Locale.getDefault()).contains(searchText)
+                            || it.vendor.lowercase(Locale.getDefault()).contains(searchText)
+                        ) {
+                            searchProducts.add(it)
+                        }
+                    }
+                    brandList()
+                } else {
+                    searchProducts.clear()
+                }
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //change when writing this the main logic
+                searchProducts.clear()
+                val searchText = newText!!.lowercase(Locale.getDefault())
+                if (searchText.isNotEmpty()) {
+                    products.forEach {
+                        if (it.title.lowercase(Locale.getDefault()).contains(searchText)
+                            || it.vendor.lowercase(Locale.getDefault()).contains(searchText)
+                        ) {
+                            searchProducts.add(it)
+                        }
+                    }
+                    brandList()
+                } else {
+                    searchProducts.clear()
+                }
+
+                return false
+            }
+
+        })
+
+    }
+    fun filter(){
+        binding.spinner.onItemSelectedListener=object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                searchProducts.clear()
+                if (parent!!.getItemAtPosition(position).equals("price From 0 to 100")){
+                    priceFilter("0","100")
+                }else if (parent!!.getItemAtPosition(position).equals("price to 150")){
+                    priceFilter("0","150")
+
+                }else if(parent!!.getItemAtPosition(position).equals("price to 200")){
+                    priceFilter("0","200")
+                }else if(parent!!.getItemAtPosition(position).equals("price to 500")){
+                    priceFilter("0","500")
+
+                }else if(parent!!.getItemAtPosition(position).equals("price to 1000")){
+                    priceFilter("0","1000")
+
+                }else if(parent!!.getItemAtPosition(position).equals("price bigger than 1000")){
+                    priceFilter("0","5000")
+
+                }
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                Log.d("yaaaaaaaa","NOTHING")
+            }
+
+        }
+    }
+    fun priceFilter(price1:String,price2: String){
+        searchProducts.clear()
+        products.forEach {
+            if (it.variants[0].price in price1..price2) {
+                searchProducts.add(it)
+            }
+        }
+        brandList()
+    }
+
 }
+
+
+
+  /*
+   //seek bar run put error on price logic
+   private fun click() {
+        binding.imageView3.setOnClickListener {
+            binding.filterPrice.visibility = View.VISIBLE
+        }
+        binding.close.setOnClickListener {
+            binding.filterPrice.visibility = View.GONE
+            //Delete result
+        }
+
+    }
+
+    private fun filter(){
+        seekBar=binding.seekBar as SeekBar
+        seekBar.progress = 15
+        seekBar.max=2000
+
+      //  seekBar.incrementProgressBy(10)
+
+
+        seekBar.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                price=progress.toString()
+                binding.tvPrice.text=progress.toString()
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                searchProducts.clear()
+                products.forEach {
+                    if (it.variants[0].price<=price) {
+                        searchProducts.add(it)
+                    }
+                }
+                brandList()
+            }
+
+        })
+
+
+    }
+*/
+
+
