71b693d3d82162ac030c1438f1dc955c00dc93b9
AyaAlaa010 <ayaalaa010920@gmail.com>
2022-03-11 16:25:39 +0200

    currancy cart 2

--- app/src/main/java/com/hema/e_commerce/ui/cart/Cart.kt
+++ app/src/main/java/com/hema/e_commerce/ui/cart/Cart.kt
@@ -1,13 +1,17 @@
 package com.hema.e_commerce.ui.cart
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hema.e_commerce.R
 import com.hema.e_commerce.adapter.cart.CartAdapter
@@ -48,7 +52,59 @@ class Cart : Fragment(){
                 cartFragmentBinding.notLoged.visibility=View.GONE
 
                 cartAdapter.differ.submitList(product)
-                cartFragmentBinding.textTotalprice.text=totalCalc(product).toString()
+                //
+
+                val sharedPreferences: SharedPreferences =requireContext().getSharedPreferences("currency", 0)
+                var value = sharedPreferences.getString("currency","EGP")
+                when(value){
+                    "EGP"-> {cartFragmentBinding.textTotalprice.text=totalCalc(product).toString()+" "+getString(R.string.eg)
+
+                        cartFragmentBinding.tvSubTotal.text=totalCalc(product).toString()+" "+getString(R.string.eg)
+
+                    }
+                    "USA"-> {
+                        var usCurrancy=   ((totalCalc(product).toString()).toDouble() / (15.71))
+                        val number:Double=String.format("%.2f",usCurrancy).toDouble()
+                        cartFragmentBinding.textTotalprice.text= number.toString() + " " + getString(R.string.us)
+                        //
+                        var usCurrancySub=   ((totalCalc(product).toString()).toDouble() / (15.71))
+                        val numberSub:Double=String.format("%.2f",usCurrancy).toDouble()
+                        cartFragmentBinding.tvSubTotal.text= number.toString() + " " + getString(R.string.us)
+
+                    }
+                    "EUR"->      {
+                        var ureCurrancy=   ((totalCalc(product).toString()).toDouble() / (17.10))
+                        val number:Double=String.format("%.2f",ureCurrancy).toDouble()
+                        cartFragmentBinding.textTotalprice.text=number.toString()+" "+getString(R.string.eur)
+                        //
+
+                        var ureCurrancySub=   ((totalCalc(product).toString()).toDouble() / (17.10))
+                        val numberSub:Double=String.format("%.2f",ureCurrancy).toDouble()
+                        cartFragmentBinding.tvSubTotal.text=number.toString()+" "+getString(R.string.eur)
+                    }
+                    else-> {
+                        cartFragmentBinding.textTotalprice.text =
+                            totalCalc(product).toString() + " " + getString(R.string.eg)
+                        cartFragmentBinding.tvSubTotal.text=totalCalc(product).toString()+" "+getString(R.string.eg)
+
+                    }
+                }
+
+                cartFragmentBinding.btCopoun.setOnClickListener {
+                    val copoun = cartFragmentBinding.edtexCopoun.text.toString()
+                    if (copoun == "hema5"){
+                        val totalPricee =  cartFragmentBinding.textTotalprice.text.toString()
+                        val discount = totalPricee.toDouble() - 20
+                        cartFragmentBinding.textTotalprice.text = discount.toString()
+                        cartFragmentBinding.tvDiscount.text = "-20"
+
+                    }
+                    else if(copoun != "hema5"){
+                        cartFragmentBinding.tvSubTotal.text=totalCalc(product).toString()
+                        cartFragmentBinding.tvDiscount.text = "0"
+                    }
+                }
+
 
             }else{
                 cartFragmentBinding.notLoged.visibility=View.VISIBLE
@@ -57,8 +113,22 @@ class Cart : Fragment(){
             }
         })
 
+        bindButton()
     }
 
+    private fun bindButton() {
+        cartFragmentBinding.btCheckout.setOnClickListener {
+            val totalPrice =cartFragmentBinding.textTotalprice.text.toString()
+            if (totalPrice.isNotEmpty()){
+                val action = CartDirections.actionCartToCheckout(totalPrice)
+                Navigation.findNavController(requireView()).navigate(action)
+            }else{
+//                Toast.makeText()
+            }
+//            findNavController().navigate(R.id.action_cart_to_checkout)
+
+        }
+    }
 
 
     private fun totalCalc(items:List<CartProductData>):Double{
